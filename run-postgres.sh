586813e68924707fa03841f5c2e5d9441909d4e9
andrew-desktop <andreynizovkin@inbox.ru>
2024-04-20 11:11:36 +0300

    Add: create db

---
+++ run-postgres.sh
@@ -0,0 +1,1 @@
+docker run -d --name postgres-container -e POSTGRES_DB=basepointsdb -e POSTGRES_USER=andrew -e POSTGRES_PASSWORD=1234 -p 5432:5432 -v /home/andrew/basepointsdb:/var/lib/postgresql/data postgres:alpine3.19
